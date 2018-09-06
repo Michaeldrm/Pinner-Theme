8834594b13b4bd88cbcf93c38b7b7d9b1fd26146
Michael Douglas Rabelo <michael.douglas@outlook.com>
2018-09-06 14:04:25 -0300

    Chages to product gallery

---
+++ assets/precache-manifest.26be0b7ac4b4e1d6e6fd13d0d900747c.js
@@ -0,0 +1,12 @@
+self.__precacheManifest = [
+  {
+    "url": "/assets/js/theme-20d595a9f0fa353255bf.js"
+  },
+  {
+    "url": "/assets/js/app-b7733a4723821628cb24.js"
+  },
+  {
+    "revision": "20d595a9f0fa353255bf",
+    "url": "/assets/css/bundle-0878cd6f3399034682ba.css"
+  }
+];
---
+++ assets/scss/theme/assets/scss/theme/assets/scss/theme/assets/scss/theme/assets/scss/theme/assets/scss/theme/assets/scss/theme/assets/scss/theme/assets/scss/theme/assets/scss/theme/assets/precache-manifest.f48ea7e6dcdb67cd85841e7216a14b45.js
@@ -0,0 +1,12 @@
+self.__precacheManifest = [
+  {
+    "url": "/assets/js/theme-b55b216833938afcc3f4.js"
+  },
+  {
+    "url": "/assets/js/app-b7733a4723821628cb24.js"
+  },
+  {
+    "revision": "b55b216833938afcc3f4",
+    "url": "/assets/css/bundle-aad17877deb7b35140f8.css"
+  }
+];
